1e956f8a0da026be376ba654da1c39195a8f6f1b
grizz03 <sawilson1110@yahoo.com>
2021-04-25 22:54:08 -0700

    turning with camera, character done

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -7,13 +7,16 @@ public class PlayerController : MonoBehaviour
     public float moveSpeed = 4f;
     public float jumpForce = 10f;
     public float gravityScale = 5f;
+    public float rotateSpeed = 0.5f;
 
     public CharacterController CharController;
     private Vector3 moveDirection;
+    private Camera theCam;
+    public GameObject playerModel;
     // Start is called before the first frame update
     void Start()
     {
-
+        theCam = Camera.main;
     }
 
     // Update is called once per frame
@@ -21,7 +24,8 @@ public class PlayerController : MonoBehaviour
     {
         float yStore = moveDirection.y;
 
-        moveDirection = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
+        // moveDirection = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
+        moveDirection = (transform.forward * Input.GetAxisRaw("Vertical")) + (transform.right * Input.GetAxisRaw("Horizontal"));
         moveDirection = moveDirection * moveSpeed;
         moveDirection.y = yStore;
 
@@ -36,5 +40,14 @@ public class PlayerController : MonoBehaviour
         // transform.position = transform.position + moveDirection * Time.deltaTime * moveSpeed;
 
         CharController.Move(moveDirection * Time.deltaTime);
+
+        //camera movement towards camera
+        if (Input.GetAxisRaw("Horizontal") != 0 || Input.GetAxisRaw("Vertical") != 0)
+        {
+            transform.rotation = Quaternion.Euler(0f, theCam.transform.rotation.eulerAngles.y, 0f);
+            Quaternion newRotation = Quaternion.LookRotation(new Vector3(moveDirection.x, 0f, moveDirection.z));
+            //playerModel.transform.rotation = newRotation;
+            playerModel.transform.rotation = Quaternion.Slerp(playerModel.transform.rotation, newRotation, rotateSpeed * Time.deltaTime);
+        }
     }
 }
