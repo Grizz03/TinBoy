f7b19adba95c7f83d73b215d98c191dda551529a
grizz03 <sawilson1110@yahoo.com>
2021-04-25 22:03:13 -0700

    setup gravity for player and movement

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,15 +4,37 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    public float moveSpeed = 4f;
+    public float jumpForce = 10f;
+    public float gravityScale = 5f;
+
+    public CharacterController CharController;
+    private Vector3 moveDirection;
     // Start is called before the first frame update
     void Start()
     {
-        
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float yStore = moveDirection.y;
+
+        moveDirection = new Vector3(Input.GetAxisRaw("Horizontal"), 0f, Input.GetAxisRaw("Vertical"));
+        moveDirection = moveDirection * moveSpeed;
+        moveDirection.y = yStore;
+
+        if (Input.GetButtonDown("Jump"))
+        {
+            moveDirection.y = jumpForce;
+        }
+
+        //added gravity to jump
+        moveDirection.y += Physics.gravity.y * Time.deltaTime * gravityScale;
+
+        // transform.position = transform.position + moveDirection * Time.deltaTime * moveSpeed;
+
+        CharController.Move(moveDirection * Time.deltaTime);
     }
 }
